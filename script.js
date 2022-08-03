11256647197e69bf142158f0b9938fc9857463f5
ThiagoCastagnazzi <thiagocastagnazzi@gmail.com>
2022-08-02 23:51:08 -0400

    feat: added robot to the table

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 var btn_create_table = document.querySelector(".btn-create-table");
+var btn_move_foward = document.querySelector(".btn-move-foward");
+var btn_move_left = document.querySelector(".btn-move-left");
+var btn_move_right = document.querySelector(".btn-move-right");
+var terrenoX = document.querySelector("#terrenoX");
+var terrenoY = document.querySelector("#terrenoY");
 btn_create_table.addEventListener("click", makerTable);
+btn_move_foward.addEventListener("click", moveFoward);
+btn_move_left.addEventListener("click", moveLeft);
+btn_move_right.addEventListener("click", moveRight);
 
 function makerTable() {
+  terrenoX = terrenoX.value;
+  terrenoY = terrenoY.value;
   let tr = document.createElement("tr");
   document.querySelector(".game-wrapper").appendChild(tr).className =
     "game-table";
-  tr.style.setProperty("grid-template-columns", `repeat(${MAP_SIZE_Y}, 1fr)`);
-  for (let i = 0; i < MAP_SIZE_X; i++) {
-    for (let j = 0; j < MAP_SIZE_Y; j++) {
+  tr.style.setProperty("grid-template-columns", `repeat(${terrenoY}, 1fr)`);
+  for (let i = terrenoX - 1; i >= 0; i--) {
+    for (let j = 0; j < terrenoY; j++) {
       let td = document.createElement("td");
       document.querySelector(".game-table").appendChild(td).className = "box";
       td.setAttribute("x", `${i}`);
       td.setAttribute("y", `${j}`);
     }
   }
+
+  let img = document.createElement("img");
+  let box = document.querySelector(
+    `.box[x="${robot1.position.x}"][y="${robot1.position.y}"]`
+  );
+  let boxChild = box.appendChild(img);
+  boxChild.id = "robot";
+  boxChild.setAttribute("src", "./robot.png");
+  boxChild.setAttribute("direction", robot1.direction);
+  console.log(box);
+}
+
+function moveFoward() {}
+
+var robot1 = {
+  position: {
+    x: 0,
+    y: 0,
+  },
+  direction: "NORTH",
+};
+
+function moveLeft() {
+  direction == "L"
+    ? (robot1.direction = coordinates[robot1.direction].LEFT)
+    : robot1.direction;
+}
+
+function moveRight() {
+  direction == "R"
+    ? (robot1.direction = coordinates[robot1.direction].RIGHT)
+    : robot1.direction;
 }
 
 var MAP_SIZE_Y = 5;
@@ -23,7 +65,7 @@ var MAP_SIZE = MAP_SIZE_X * MAP_SIZE_Y;
 const coordinates = {
   NORTH: { LEFT: "WEST", RIGHT: "EAST" },
   EAST: { LEFT: "NORTH", RIGHT: "SOUTH" },
-  SOUTH: { LEFT: "", RIGHT: "WEST" },
+  SOUTH: { LEFT: "EAST", RIGHT: "WEST" },
   WEST: { LEFT: "SOUTH", RIGHT: "NORTH" },
 };
 
