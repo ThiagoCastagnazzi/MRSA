30df4771ce57aaa4784719f9b4f62d1bb273fae1
ThiagoCastagnazzi <thiagocastagnazzi@gmail.com>
2022-08-04 22:49:59 -0400

    feat: add function to move the robot when send a command and run

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ var mapY = mapYValue.value;
 var robot_positionX = document.getElementById("robot-position-X");
 var robot_positionY = document.getElementById("robot-position-Y");
 var robot_direction = document.getElementById("robot-direction");
+var robot_command = document.getElementById("robot-command");
 
-var robot1 = {
+var robot = {
   position: {
     x: 0,
     y: 0,
   },
   direction: "NORTH",
+};
+
+const coordinates = {
+  NORTH: { LEFT: "WEST", RIGHT: "EAST" },
+  EAST: { LEFT: "NORTH", RIGHT: "SOUTH" },
+  SOUTH: { LEFT: "EAST", RIGHT: "WEST" },
+  WEST: { LEFT: "SOUTH", RIGHT: "NORTH" },
 };
 
 const myRobot = document.createElement("img");
@@ -20,16 +28,16 @@ myRobot.setAttribute("src", "./robot.png");
 createGameMap();
 
 function robotPosition() {
-  robot_positionX.innerHTML = `X: ${robot1.position.x}`;
-  robot_positionY.innerHTML = `Y: ${robot1.position.y}`;
-  robot_direction.innerHTML = `Direction: ${robot1.direction}`;
+  robot_positionX.innerHTML = `X: ${robot.position.x}`;
+  robot_positionY.innerHTML = `Y: ${robot.position.y}`;
+  robot_direction.innerHTML = `Direction: ${robot.direction}`;
 }
 
 function render() {
   document
-    .getElementById(robot1.position.y + "x" + robot1.position.x)
+    .getElementById(robot.position.y + "x" + robot.position.x)
     .appendChild(myRobot);
-  myRobot.setAttribute("direction", robot1.direction);
+  myRobot.setAttribute("direction", robot.direction);
 }
 
 function createGameMap() {
@@ -55,17 +63,38 @@ function createGameMap() {
   render();
 }
 
+function runCommand() {
+  var dados = robot_command.value;
+  dados = dados.replace(/\s+/g, "").toUpperCase();
+  for (let i = 0; i < dados.length; i++) {
+    getCommand(dados[i]);
+    document.getElementById("robot-command").value = "";
+  }
+}
+
+function getCommand(dado) {
+  dado !== "L" && dado !== "R" && dado !== "M"
+    ? alert(`Caracter ${dado} inválido`)
+    : dado === "L"
+    ? moveLeft()
+    : dado === "R"
+    ? moveRight()
+    : dado === "M"
+    ? robotMove()
+    : "";
+}
+
 function robotRotate() {
-  document.getElementById(robot1.position.y + "x" + robot1.position.x);
-  myRobot.setAttribute("direction", robot1.direction);
+  document.getElementById(robot.position.y + "x" + robot.position.x);
+  myRobot.setAttribute("direction", robot.direction);
   myRobot.style.setProperty(
     "transform",
     `rotate(${
-      robot1.direction == "NORTH"
+      robot.direction == "NORTH"
         ? 0
-        : robot1.direction == "EAST"
+        : robot.direction == "EAST"
         ? 90
-        : robot1.direction == "SOUTH"
+        : robot.direction == "SOUTH"
         ? 180
         : 270
     }deg)`
@@ -74,18 +103,18 @@ function robotRotate() {
 }
 
 function robotMove() {
-  switch (robot1.direction) {
+  switch (robot.direction) {
     case "NORTH":
-      robot1.position.y + 1 < mapY ? robot1.position.y++ : robot1.position.y;
+      robot.position.y + 1 < mapY ? robot.position.y++ : robot.position.y;
       break;
     case "EAST":
-      robot1.position.x + 1 < mapX ? robot1.position.x++ : robot1.position.x;
+      robot.position.x + 1 < mapX ? robot.position.x++ : robot.position.x;
       break;
     case "SOUTH":
-      robot1.position.y - 1 >= 0 ? robot1.position.y-- : robot1.position.y;
+      robot.position.y - 1 >= 0 ? robot.position.y-- : robot.position.y;
       break;
     case "WEST":
-      robot1.position.x - 1 >= 0 ? robot1.position.x-- : robot1.position.x;
+      robot.position.x - 1 >= 0 ? robot.position.x-- : robot.position.x;
       break;
 
     default:
@@ -93,29 +122,29 @@ function robotMove() {
   }
 
   document
-    .getElementById(robot1.position.y + "x" + robot1.position.x)
+    .getElementById(robot.position.y + "x" + robot.position.x)
     .appendChild(myRobot);
-  myRobot.setAttribute("direction", robot1.direction);
+  myRobot.setAttribute("direction", robot.direction);
   myRobot.style.setProperty(
     "transform",
     `rotate(${
-      robot1.direction == "NORTH"
+      robot.direction == "NORTH"
         ? 0
-        : robot1.direction == "EAST"
+        : robot.direction == "EAST"
         ? 90
-        : robot1.direction == "SOUTH"
+        : robot.direction == "SOUTH"
         ? 180
         : 270
     }deg)`
   );
   robotPosition();
-  console.log(`X: ${robot1.position.x} Y: ${robot1.position.y}`);
+  console.log(`X: ${robot.position.x} Y: ${robot.position.y}`);
 }
 
 function limparTabela() {
-  robot1.position.x = 0;
-  robot1.position.y = 0;
-  robot1.direction = "NORTH";
+  robot.position.x = 0;
+  robot.position.y = 0;
+  robot.direction = "NORTH";
   robotRotate();
   robotPosition();
   document.querySelectorAll("#gameMap  tbody tr").forEach((a) => {
@@ -124,73 +153,11 @@ function limparTabela() {
 }
 
 function moveLeft() {
-  robot1.direction = coordinates[robot1.direction].LEFT;
+  robot.direction = coordinates[robot.direction].LEFT;
   robotRotate();
 }
 
 function moveRight() {
-  robot1.direction = coordinates[robot1.direction].RIGHT;
+  robot.direction = coordinates[robot.direction].RIGHT;
   robotRotate();
 }
-
-// NODE EXEC BELOW
-
-var MAP_SIZE_Y = 5;
-var MAP_SIZE_X = 5;
-var MAP_SIZE = MAP_SIZE_X * MAP_SIZE_Y;
-
-const coordinates = {
-  NORTH: { LEFT: "WEST", RIGHT: "EAST" },
-  EAST: { LEFT: "NORTH", RIGHT: "SOUTH" },
-  SOUTH: { LEFT: "EAST", RIGHT: "WEST" },
-  WEST: { LEFT: "SOUTH", RIGHT: "NORTH" },
-};
-
-var robot = {
-  position: {
-    x: 0,
-    y: 0,
-  },
-  direction: "NORTH",
-};
-
-function move(direction) {
-  direction == "NORTH" && robot.position.y + 1 < MAP_SIZE_Y
-    ? robot.position.y++
-    : robot.position.y;
-  direction == "EAST" && robot.position.x + 1 < MAP_SIZE_X
-    ? robot.position.x++
-    : robot.position.x;
-  direction == "SOUTH" && robot.position.y - 1 >= 0
-    ? robot.position.y--
-    : robot.position.y;
-  direction == "WEST" && robot.position.x - 1 >= 0
-    ? robot.position.x--
-    : robot.position.x;
-}
-
-function rotate(direction) {
-  direction == "R"
-    ? (robot.direction = coordinates[robot.direction].RIGHT)
-    : robot.direction;
-  direction == "L"
-    ? (robot.direction = coordinates[robot.direction].LEFT)
-    : robot.direction;
-}
-
-function main(dados) {
-  dados = dados.replace(/\s+/g, "").toUpperCase();
-  for (let i = 0; i < dados.length; i++) {
-    dados[i] != "L" && dados[i] != "R" && dados[i] != "M"
-      ? console.log(`Caracter ${dados[i]} inválido`)
-      : "";
-    dados[i] == "L" || "R" ? rotate(dados[i]) : "";
-    dados[i] == "M" ? move(robot.direction) : "";
-    console.log("{dado:" + dados[i] + "}");
-    console.log(`Posição: ${robot.position.x}x${robot.position.y}`);
-    console.log(`Direção: ${robot.direction}`);
-    console.log("\n");
-  }
-}
-
-main("MML");
